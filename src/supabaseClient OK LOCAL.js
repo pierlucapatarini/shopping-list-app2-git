ecf71dcfe5716e80b4466f0d9c7d0024194dbc51
pierlucapatarini <pierluca.patarini@icloud.com>
2025-09-11 17:13:54 +0200

    mofdifica supabaseclients

---
+++ src/supabaseClient OK LOCAL.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://gvcdchndpofcfdvhpyos.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Y2RjaG5kcG9mY2ZkdmhweW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDA2MzUsImV4cCI6MjA3MjM3NjYzNX0.KiAB8mRE6EvGTS6SC7cDAOW97co7wgQZeU3D7BGFugs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gvcdchndpofcfdvhpyos.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Y2RjaG5kcG9mY2ZkdmhweW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MDA2MzUsImV4cCI6MjA3MjM3NjYzNX0.KiAB8mRE6EvGTS6SC7cDAOW97co7wgQZeU3D7BGFugs'
+// Usa le variabili d'ambiente. Il prefisso REACT_APP_ è necessario per le app client.
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-console.log("URL:", process.env.REACT_APP_SUPABASE_URL);
-console.log("KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY);
-
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
